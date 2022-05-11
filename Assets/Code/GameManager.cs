b18ef5e1f09e53f0618fd87e4a3c09661763f192
You Jin Song <ysong.yjs@gmail.com>
2022-05-11 15:51:52 -0400

    changed modern light to torch + added post processing + changed the gun modeling

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
+    public Volume volume;
+    public Vignette vignette;
     int towerInGame = 1;
     
-    int[] towerHealth;
+    public int[] towerHealth;
     public RectTransform[] healthBars;
+    bool hurt = false;
 
     void Start()
     {
+        volume = GetComponent<Volume>();
+        volume.profile.TryGet(out vignette);
+
         towerHealth = new int[towerInGame];
         for (int i = 0; i < towerHealth.Length; i++)
         {
@@ -20,9 +28,18 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (hurt == true)
+        {
+            StartCoroutine(HurtEffect());
+        }
+    }
+
     public bool UpdateHealth(int hitTower, int damage)
     {
         towerHealth[hitTower] -= damage;
+        hurt = true;
         healthBars[hitTower].localScale = new Vector3(towerHealth[hitTower] * .01f, 1, 1);
         
         if (towerHealth[hitTower] > 0)
@@ -38,11 +55,18 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    IEnumerator HurtEffect() {
+        vignette.intensity.value = 0.18f;
+        yield return new WaitForSeconds(0.5f);
+        vignette.intensity.value = 0f;
+    }
+
+
     void CheckRoundOver()
     {
         if (towerInGame < 1)
         {
-            SceneManager.LoadScene(DeadScene);
+            SceneManager.LoadScene("DeadScene");
         }
     }
 }
